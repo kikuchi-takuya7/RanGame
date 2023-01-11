cc796f882a45b55880242bfe6b5fba7e663e2be7
巧椰 菊地 <220065@jc-21.jp>
2023-01-11 13:27:09 +0900

    背景とかプレイヤーを作った

--- Main.cpp
+++ Main.cpp
@@ -3,10 +3,20 @@
 void Main()
 {
 	
+	const Texture background{ U"example/sky002_day.jpg" };
+	double x = 0.0;
 
 	while (System::Update())
 	{
-		Circle{ 400, 300, 20 }.draw();
+		
+		x += (Scene::DeltaTime() * 180);
+		background.draw(x, 0);
+		Circle{ x, 100, 20 }.draw(Palette::Black);
+		Vec2 player = {100, 500};
+
+		Circle{ player.x, player.y, 20 }.draw(Palette::Black);
+		Rect{ 0, 520, 1000, 10 }.draw(Palette::Black);
+
 	}
 }
 
